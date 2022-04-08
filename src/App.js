dbf707e03874f10f33f21c455510002b3cb4de85
blahkbyrd <julie-trinchero@blahkbyrd.fr>
2022-04-08 22:28:07 +0200

    add a section for containing the aside and the pages

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ import Home from './Pages/Home';
 import Events from './Pages/Events';
 import Radio from './Pages/Radio';
 import Contact from './Pages/Contact';
-
+import Aside from './Components/Aside';
 function App() {
   return (
     <Router className="App">
       <Header />
+      <section className='body-container'>
+      <Aside/>
       <Routes >
       <Route path="/" element={<Home />} />
       <Route path="/events" element={<Events />} />
       <Route path="/radio" element={<Radio/>} />
       <Route path="/contact" element={<Contact />} />
       </Routes>
+      </section>
       <Footer />
     </Router>
   );
